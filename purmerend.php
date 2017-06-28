4f1a3b7f3991c994dbcf91ed9dffa1721456821e
Willem Liu <willem_liu@hotmail.com>
2017-06-28 21:23:43 +0200

    require

--- purmerend.php
+++ purmerend.php
@@ -1,2 +1,5 @@
 <?php
-require_once('index.php');
+$file = basename(__FILE__);
+$city = (strcasecmp(basename(__FILE__, '.php'), 'index') == 0) ? 'Amsterdam' : ucfirst(basename(__FILE__, '.php'));
+
+require_once('include.php');
